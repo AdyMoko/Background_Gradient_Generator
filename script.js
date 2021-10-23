12d582747b1314c0857385e53b8d885381878fd9
AdyMoko <mocodean_adrian@yahoo.com>
2021-10-23 12:38:03 +0300

    added a refined randomize function

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 var button = document.getElementById("button");
 
-
-
 setGradient();
 
 function setGradient() {
@@ -15,15 +13,26 @@ function setGradient() {
 	+ ", " 
 	+ color2.value 
 	+ ")";
-
 	css.textContent = body.style.background + ";";
 }
 
 function gigi(){
-	color1.value = "#" + Math.floor(Math.random() * 999999);	
+	color1.value = "#" + getRandomHex();	
 	setGradient();
-	color2.value = "#" + Math.floor(Math.random() * 999999);
+	color2.value = "#" + getRandomHex();
 	setGradient();
+}
+
+let getRandomHex = function(){
+    const array = [1,2,3,4,5,6,7,8,9,"A", "B", "C", "D", "E", "F"];
+    let answer = []; 
+    for (i=0; i<6; i++) {   
+            let result = Math.floor(Math.random() *array.length);  
+            let y = array[result];
+            answer.push(y);    
+    }
+    let finalAnswer = answer.toString().replace(/,/g, "");
+    return finalAnswer;
 }
 
 
